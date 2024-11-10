5e923772a3a32071c49ae25eca32f8c4ac1fdb12
sapan <sapanshah2468@gmail.com>
2024-11-10 16:21:10 -0500

    Added basic recommendation API route

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to the Movie Recommendation System!"
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    # Placeholder for actual recommendation logic
+    data = request.get_json()
+    user_id = data.get('userId')
+    return jsonify({'recommendations': [f'Movie {i}' for i in range(1, 6)]})
 
 if __name__ == '__main__':
     app.run(debug=True)
